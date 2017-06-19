207a56ebd07d509100d613dfdcb62b03f3b010d4
BerniceChua <Bernice.Chua.415@gmail.com>
2017-06-18 23:47:31 -0700

    Fix the bug that causes the game piece to get stuck on the right side of the screen.

--- Assets/Scripts/Orbit.cs
+++ Assets/Scripts/Orbit.cs
@@ -2,28 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Orbit : MonoBehaviour {
+public class Orbit : MonoBehaviour
+{
 
     public Transform target;
-    [SerializeField] float m_speedMultiplier = 10.0f;
-    [SerializeField] float m_slideBackwards = 1.0f;
+    [SerializeField]
+    float m_speedMultiplier = 10.0f;
+    [SerializeField]
+    float m_slideBackwards = 1.0f;
+    [SerializeField]
+    Camera m_camera;
+    Vector3 m_screenPosition;
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        m_screenPosition = m_camera.WorldToViewportPoint(this.transform.position);
         //speed = GetSpeed();
 
         //transform.RotateAround(target.position, target.up, speed * Time.deltaTime);
 
-        if (this.enabled) {
-            float speedOfGamePiece = GetSpeed();
+        if (this.enabled)
+        {
+            //float speedOfGamePiece = GetSpeed();
 
-            //transform.RotateAround(target.position, target.up, speedOfGamePiece);
-            transform.RotateAround(target.position, Vector3.up, -m_slideBackwards + speedOfGamePiece);
+            Vector3 targetDir = target.position - transform.position;
+
+            float angle = Vector3.Angle(targetDir, transform.forward);
+
+            //if (m_screenPosition.x < 1.0f) {
+            //if (angle <= 45 && angle >= -45) {
+                //transform.RotateAround(target.position, target.up, speedOfGamePiece);
+                //transform.RotateAround(target.position, Vector3.up, -m_slideBackwards + speedOfGamePiece);
+                //transform.RotateAround(target.position, Vector3.right, -m_slideBackwards + speedOfGamePiece);
+                transform.RotateAround(m_camera.transform.position, Vector3.up, ClampMovements());
+            //}
         }
     }
 
@@ -36,5 +55,17 @@ public class Orbit : MonoBehaviour {
         float speed = Mathf.Clamp(Mathf.Abs(Input.gyro.attitude.x) * m_speedMultiplier, 0.0f, 1.5f);
 
         return speed;
+    }
+
+    public float ClampMovements() {
+        //return Mathf.Clamp(m_screenPosition.x, 0.0f, 1.1f);
+
+        if (m_screenPosition.x > 1.1f) {
+            return -m_slideBackwards * 1.2f;
+        } else if (m_screenPosition.x < 1.1f) {
+            return -m_slideBackwards + GetSpeed();
+        } else {
+            return -m_slideBackwards;
+        }
     }
 }
