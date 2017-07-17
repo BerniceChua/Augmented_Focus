3d93df2bf97a5cb36f522ee58e492aec702eb827
BerniceChua <Bernice.Chua.415@gmail.com>
2017-07-17 00:56:25 -0700

    Upload/modify Events.cs, to make it for 'Augmented Focus' instead of Dizzy.

---
+++ Assets/Scripts/Events.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Events {
+    public float length;
+    public List<UnityAction> actions;
+    public UnityAction endAction;
+    public string text;
+
+    public static List<GameObject> eventObjects = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //public Event(flo)
+
+}
