2c60cc7a95e39f44e5654e943df8a2be4e808591
BerniceChua <Bernice.Chua.415@gmail.com>
2017-07-16 21:39:52 -0700

    Add TestVector3RotateTowrds.cs to check if it is something that will work with this project.

---
+++ Assets/Scripts/TestVector3RotateTowards.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestVector3RotateTowards : MonoBehaviour {
+    public Transform target;
+    public float speed;
+    void Update() {
+        Vector3 targetDir = target.position - transform.position;
+        float step = speed * Time.deltaTime;
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+        Debug.DrawRay(transform.position, newDir, Color.red);
+        transform.rotation = Quaternion.LookRotation(newDir);
+    }
+}
