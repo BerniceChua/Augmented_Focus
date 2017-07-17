847c130a0e3f6c9154ee4987045c093ec21b6662
BerniceChua <Bernice.Chua.415@gmail.com>
2017-07-17 02:33:09 -0700

    Add WingAnimationSpeed.cs to control the wings flapping animation movement speed.

---
+++ Assets/Scripts/WingAnimationSpeed.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WingAnimationSpeed : MonoBehaviour {
+    [SerializeField] float m_speed = 99999.0f;
+
+    Animator m_hummingbirdAnimator { get { return GetComponent<Animator>(); } set { m_hummingbirdAnimator = value; } }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //GetComponent<Animation>().Play("walk");
+        m_hummingbirdAnimator.speed = m_speed;
+    }
+}
