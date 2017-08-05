0e36d609d6ca932179e15fa7f875e27703e958c9
BerniceChua <Bernice.Chua.415@gmail.com>
2017-08-04 18:54:04 -0700

    Animate InstructionsPicture (4) with CycleThroughImages.cs.

--- Assets/Scripts/CycleThroughImages.cs
+++ Assets/Scripts/CycleThroughImages.cs
@@ -17,31 +17,26 @@ public class CycleThroughImages : MonoBehaviour {
         for (int i = 0; i < transform.childCount; i++) {
             m_imagesList[i] = transform.GetChild(i).gameObject;
             m_imagesList[i].SetActive(false);
-            Debug.Log(m_imagesList[i]);
+            //Debug.Log(m_imagesList[i]);
         }
 
         if (m_imagesList[m_index])
             m_imagesList[m_index].SetActive(true);
 
-        Debug.Log(m_imagesList);
-        Debug.Log(transform);
-        Debug.Log(transform.childCount);
+        //Debug.Log(m_imagesList);
+        //Debug.Log(transform);
+        //Debug.Log(transform.childCount);
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-        WhileThisSectionIsActive();
-    }
-
-    private void WhileThisSectionIsActive() {
-        while (m_parentPanel.activeInHierarchy)
-            NextImage();
+        StartCoroutine(NextImage());
     }
 
     private IEnumerator NextImage() {
         yield return new WaitForSecondsRealtime(m_timeToWait);
-        Debug.Log("I'm at the next picture!");
+        //Debug.Log("I'm at the next picture!");
 
         m_imagesList[m_index].SetActive(false);
 
