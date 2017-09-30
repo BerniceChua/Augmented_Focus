c541894d3a80cff55d919698daed7f6391c6fd65
BerniceChua <Bernice.Chua.415@gmail.com>
2017-09-30 00:00:25 -0700

    Add code to make Sparkles work when time interval arrives and difficulty increases (FINALLYgit status).

--- Assets/Scripts/AddSparklesBasedOnTimer.cs
+++ Assets/Scripts/AddSparklesBasedOnTimer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AddSparklesBasedOnTimer : MonoBehaviour {
 
@@ -23,11 +24,11 @@ public class AddSparklesBasedOnTimer : MonoBehaviour {
     [SerializeField] Sprite[] m_sparkleSpritesArray;
 
 
-    GameObject sparkleInstance;
+    GameObject m_sparkleInstance;
 
     // Use this for initialization
     void Start () {
-        print("m_imagesArray.Length = " + m_imagesArray.Length);
+        print("m_sparkleSpritesArray.Length = " + m_sparkleSpritesArray.Length);
     }
 	
 	// Update is called once per frame
@@ -40,14 +41,21 @@ public class AddSparklesBasedOnTimer : MonoBehaviour {
         Sprite sparkleSprite = m_sparkleSpritesArray[arrayIndex];
         string sparkleSpriteName = sparkleSprite.name;
 
-        sparkleInstance = Instantiate(m_sparkleImagePrefab);
-        sparkleInstance.name = sparkleSpriteName;
-        sparkleInstance.GetComponent<SparkleSpriteImage>().m_sparkleSpriteName = sparkleSpriteName;
-        sparkleInstance.GetComponent<SpriteRenderer>().sprite = sparkleSprite;
+        //sparkleInstance = Instantiate(m_sparkleImagePrefab);
+        m_sparkleInstance = Instantiate(m_sparkleImagePrefab, m_imagesContainerTransform, false);
+        m_sparkleInstance.transform.SetParent(m_imagesContainerTransform, false);
+        m_sparkleInstance.GetComponent<RectTransform>().position = new Vector3(7.629395e-06f, -2.288818e-05f, 0);
+        m_sparkleInstance.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
+        m_sparkleInstance.name = sparkleSpriteName;
+        m_sparkleInstance.GetComponent<SparkleSpriteImage>().m_sparkleSpriteName = sparkleSpriteName;
+        m_sparkleInstance.GetComponent<SpriteRenderer>().sprite = sparkleSprite;
+        //sparkleInstance.GetComponent<Image>().sprite = sparkleSprite;
+
+        print("Inside 'GetRandomSparkleSprite()' function.");
     }
 
     public float GetTime() {
-        //Debug.Log("m_timeElapsed.Timer() = " + m_timeElapsed.Timer());
+        Debug.Log("m_timeElapsed.Timer() = " + m_timeElapsed.Timer());
         return m_timeElapsed.Timer();
     }
 
@@ -55,7 +63,7 @@ public class AddSparklesBasedOnTimer : MonoBehaviour {
         if (m_detectGameOver.enabled == true) {
             if (GetTime() >= 1.0f) {
                 if (Mathf.Floor(GetTime()) % m_timeInterval == 0) {
-                    //Debug.Log("Mathf.Floor(GetTime()) = " + Mathf.Floor(GetTime()));
+                    Debug.Log("Mathf.Floor(GetTime()) = " + Mathf.Floor(GetTime()));
 
                     StartCoroutine(AddSparkles());
                 }
@@ -65,15 +73,27 @@ public class AddSparklesBasedOnTimer : MonoBehaviour {
 
     IEnumerator AddSparkles() {
         print("Inside 'AddSparkles()' IEnumerator.");
-        m_index = Random.Range(0, m_imagesArray.Length);
+        m_index = Random.Range(0, m_sparkleSpritesArray.Length);
+        Sprite sparkleSprite = m_sparkleSpritesArray[m_index];
 
         //sparkleInstance.GetComponent<SpriteRenderer>().enabled = true;
         //GetRandomSparkleSprite();
         //yield return new WaitForSecondsRealtime(3);
         //sparkleInstance.GetComponent<SpriteRenderer>().enabled = false;
 
-        GetRandomSparkleSprite();
+        //GetRandomSparkleSprite();
+        //yield return new WaitForSecondsRealtime(3);
+        //Destroy(m_sparkleInstance);
+
+        print("sparkleSprite.name = " + sparkleSprite.name);
+
+        m_sparkleImagePrefab.SetActive(true);
+        //m_sparkleImagePrefab.GetComponent<SpriteRenderer>().sprite = sparkleSprite;
+        m_sparkleImagePrefab.GetComponent<Image>().sprite = sparkleSprite;
         yield return new WaitForSecondsRealtime(3);
-        Destroy(sparkleInstance);
+        m_sparkleImagePrefab.SetActive(false);
+
+        print("After the yield return new inside 'AddSparkles()' IEnumerator.");
+
     }
 }
