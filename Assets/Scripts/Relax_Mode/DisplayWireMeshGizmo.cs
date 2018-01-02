fbb607aff2fdc49514be07401f9a8aa4c8c9d6d9
BerniceChua <Bernice.Chua.415@gmail.com>
2018-01-01 19:09:29 -0800

    Create script for DisplayWireMeshGizmo.cs, so that it can show things with 'transparent' textures.

---
+++ Assets/Scripts/Relax_Mode/DisplayWireMeshGizmo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayWireMeshGizmo : MonoBehaviour {
+
+    Mesh m_thisMesh { get { return GetComponent<Mesh>(); } set { value = m_thisMesh; } }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireMesh(m_thisMesh);
+    }
+
+}
