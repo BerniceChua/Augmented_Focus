7f6d8911d8afcf26b3730b02ae7f8cd05b23ffde
BerniceChua <Bernice.Chua.415@gmail.com>
2018-01-02 01:58:46 -0800

    Create script for 'ScanDecision' decision-ScriptableObject.

---
+++ Assets/Scripts/Relax_Mode/ScriptableObjects/ScanDecision.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "PluggableAI/Decisions/Scan1")]
+public class ScanDecision : Decision {
+
+    //// Use this for initialization
+    //void Start () {
+
+    //}
+
+    //// Update is called once per frame
+    //void Update () {
+
+    //}
+
+    public override bool Decide(StateController controller) {
+        bool noFoodInSight = Scan(controller);
+        return noFoodInSight;
+    }
+
+    private bool Scan(StateController controller) {
+        //controller.navMeshAgent.Stop();
+        //controller.transform.Rotate(0, controller.enemyStats.searchingTurnSpeed * Time.deltaTime, 0);
+        //return controller.CheckIfCountdownElapsed(controller.enemyStats.searchDuration);
+        return FoodInTheScene.GetNearestInList().activeSelf;
+    }
+
+}
