dae103a7fe49109312acea591e0693854c28acf3
BerniceChua <Bernice.Chua.415@gmail.com>
2018-01-02 00:06:28 -0800

    Create script for 'FoodWasEatenDecision' decision-ScriptableObject.

---
+++ Assets/Scripts/Relax_Mode/ScriptableObjects/FoodWasEatenDecision.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PluggableAI/Decisions/FoodWasEatenDecision")]
+public class FoodWasEatenDecision : Decision {
+
+    public override bool Decide(StateController controller) {
+        //throw new NotImplementedException();
+
+        if (FoodInTheScene.CountThingsInFoodList() != 0 || controller.DetectFood().activeSelf == false) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
