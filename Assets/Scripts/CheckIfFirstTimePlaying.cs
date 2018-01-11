388614fa6f081a0d45e9049a5615ff00a6c48a13
BerniceChua <Bernice.Chua.415@gmail.com>
2018-01-11 03:09:10 -0800

    Modify CheckIfFIrstTimePlaying.cs so that it can also be used in other scenes, like the Intro scene, and the other modes (through Inheritance).

--- Assets/Scripts/CheckIfFirstTimePlaying.cs
+++ Assets/Scripts/CheckIfFirstTimePlaying.cs
@@ -6,31 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class CheckIfFirstTimePlaying : MonoBehaviour {
 
-    [SerializeField] GameObject m_instructionsPanel;
-    [SerializeField] GameObject m_introPanel;
-
     string m_didGameRunBefore = "autoStartTutorial";
+    string m_didBumbleBeeGetUnlocked = "unlockBumbleBee";
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         if (!PlayerPrefs.HasKey(m_didGameRunBefore)) {
             PlayerPrefs.SetInt(m_didGameRunBefore, 0);
+        }
+
+        if (!PlayerPrefs.HasKey(m_didBumbleBeeGetUnlocked)) {
+            PlayerPrefs.SetInt(m_didBumbleBeeGetUnlocked, 0);
         }
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (PlayerPrefs.GetInt(m_didGameRunBefore) == 0) {
-            AutoStartTheTutorial();
+            
         }
 	}
-
-    public void AutoStartTheTutorial() {
-        //if (m_introPanel.activeInHierarchy)
-        //    m_introPanel.SetActive(false);
-
-        m_instructionsPanel.SetActive(true);
-    }
 
     public void SetFlagThatFirstPlayWasComplete() {
         PlayerPrefs.SetInt(m_didGameRunBefore, 1);
@@ -41,14 +36,9 @@ public class CheckIfFirstTimePlaying : MonoBehaviour {
     public void ResetFlagThatFirstPlayWasComplete() {
         PlayerPrefs.SetInt(m_didGameRunBefore, 0);
 
+        PlayerPrefs.SetInt(m_didBumbleBeeGetUnlocked, 0);
+
         PlayerPrefs.Save();
-    }
-
-    public void StartMainGame() {
-        //if (PlayerPrefs.GetInt(m_didGameRunBefore) == 0 && m_instructionsPanel.activeInHierarchy)
-        //    m_instructionsPanel.SetActive(false);
-
-        m_introPanel.SetActive(true);
     }
 
 }
