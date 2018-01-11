113368cc322cbbc3abb37c52e266139808bf9608
BerniceChua <Bernice.Chua.415@gmail.com>
2018-01-10 23:45:25 -0800

    Create script for loading screen 'ClickToLoadAsync.cs'.

---
+++ Assets/Scripts/UI-Related/ClickToLoadAsync.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ClickToLoadAsync : MonoBehaviour {
+
+    public Slider m_LoadingBar;
+    public GameObject m_LoadingImage;
+
+    private AsyncOperation m_async;
+
+    public void ClickAsync(Scene sceneToLoad) {
+        m_LoadingImage.SetActive(true);
+        StartCoroutine( LoadLevelWithProgressBar(sceneToLoad) );
+    }
+
+    IEnumerator LoadLevelWithProgressBar(Scene sceneToLoad) {
+        m_async = SceneManager.LoadSceneAsync(sceneToLoad.name.ToString());
+
+        while (!m_async.isDone) {
+            m_LoadingBar.value = m_async.progress;
+            yield return null;
+        }
+    }
+
+}
